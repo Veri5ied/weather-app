8c9eb5464176fe6b161c679d7e10aad4a79de06d
Veri5ied <43749581+Veri5ied@users.noreply.github.com>
2020-09-10 15:21:49 +0100

    final touches

--- src/components/weather/WeatherDisplay.js
+++ src/components/weather/WeatherDisplay.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const WeatherDisplay = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="wrapper">
+      <div className="main">
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-5 title-container">
+              <Title />
+            </div>
+            <div className="col-xs-7 form-container">
+              {}
+              <Form fetchWeather={fetchWeather} />
+              {}
+              <Weather
+                temp={temp}
+                city={city}
+                country={country}
+                humidity={humidity}
+                desc={desc}
+                error={error}
+              />
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default WeatherDisplay;
